eb821b45658005ee95b5ad4d0fd7dba310b623f4
dveeale <https://github.com/dveeale/test2>
2016-12-30 20:26:56 +0800

    +新闻列表选项页使用databinding,复用这里有些问题

--- app/src/main/java/com/home/news/NewsAdapter.java
+++ app/src/main/java/com/home/news/NewsAdapter.java
@@ -2,6 +2,8 @@ package com.home.news;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 import com.bean.MainItemBean;
 import com.example.dveeale.sharkmvpdemo.R;
+import com.example.dveeale.sharkmvpdemo.databinding.NewsItemBindingLayoutBinding;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.network.Urls;
 import com.refreshview.recyclerview.BaseRecyclerAdapter;
@@ -30,7 +33,7 @@ import java.util.List;
 /**
  * Author： dveeale
  */
-public class NewsAdapter extends BaseRecyclerAdapter<CommonRecyclerViewHolder> {
+public class NewsAdapter extends BaseRecyclerAdapter<NewsAdapter.ItemNewsHolder> {
 
     private Context context;
 
@@ -45,25 +48,28 @@ public class NewsAdapter extends BaseRecyclerAdapter<CommonRecyclerViewHolder> {
     }
 
     @Override
-    public CommonRecyclerViewHolder getViewHolder(View view) {
-        return new CommonRecyclerViewHolder(view);
+    public ItemNewsHolder getViewHolder(View view) {
+        return new ItemNewsHolder(view);
+    }
+
+
+    @Override
+    public ItemNewsHolder onCreateViewHolder(ViewGroup parent, int viewType, boolean isItem) {
+        View view = LayoutInflater.from(context).inflate(R.layout.news_item_binding_layout, parent, false);
+        return new ItemNewsHolder(view);
     }
 
     @Override
-    public CommonRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType, boolean isItem) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_news_layout, parent, false);
-        return new CommonRecyclerViewHolder(view);
-    }
+    public void onBindViewHolder(ItemNewsHolder holder, int position, boolean isItem) {
 
-    @Override
-    public void onBindViewHolder(CommonRecyclerViewHolder holder, int position, boolean isItem) {
-        CommonRecyclerViewHolder viewHolder=holder;
-        final NewsItem mNewsItem = mNewsDatas.get(position);
-
-        viewHolder.setText(R.id.NewsTitleTextView,mNewsItem.getTitle())
-                .setText(R.id.NewsSourceTextView,mNewsItem.getSource())
-                .setText(R.id.NewsDateTextView,mNewsItem.getDate())
-                .setDraweeView(R.id.NewsDraweeView,mNewsItem.getmImages().get(0).getSrc(),(int)DpPxConvert.dip2px(context, 70));
+        holder.bind(mNewsDatas.get(position));
+        holder.setImage(mNewsDatas.get(position).getmImages().get(0).getSrc(),(int)DpPxConvert.dip2px(context, 70));
+        //CommonRecyclerViewHolder viewHolder=holder;
+        //final NewsItem mNewsItem = mNewsDatas.get(position);
+        //viewHolder.setText(R.id.NewsTitleTextView,mNewsItem.getTitle())
+        //        .setText(R.id.NewsSourceTextView,mNewsItem.getSource())
+        //        .setText(R.id.NewsDateTextView,mNewsItem.getDate())
+        //        .setDraweeView(R.id.NewsDraweeView,mNewsItem.getmImages().get(0).getSrc(),(int)DpPxConvert.dip2px(context, 70));
     }
 
 
@@ -74,6 +80,35 @@ public class NewsAdapter extends BaseRecyclerAdapter<CommonRecyclerViewHolder> {
             size=mNewsDatas.size();
         }
         return size;
+    }
+
+    public static class ItemNewsHolder extends RecyclerView.ViewHolder{
+
+        private NewsItemBindingLayoutBinding mBinding;
+
+        public ItemNewsHolder(@NonNull View itemView) {
+            super(itemView);
+            try {
+                mBinding= DataBindingUtil.bind(itemView);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void bind(@NonNull NewsItem newsItem){
+            mBinding.setNewsItem(newsItem);
+
+
+
+
+
+
+
+        }
+
+        public void setImage(String url,int width){
+            ControllerListenerUtil.setControllerListener(mBinding.NewsDraweeView, url, width);
+        }
     }
 
 }
